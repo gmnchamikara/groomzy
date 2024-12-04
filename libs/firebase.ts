e799dc739bc5ecef6d9ed792f03b49512f403a90
Nuwan Chamikara <www.gmnuwanchamikara@gmail.com>
2024-12-04 21:26:21 +0530

    fix

--- libs/firebase.ts
+++ libs/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -8,9 +8,9 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBErHLsu4P2GkxJanqVPYLtgFftThZvmAM",
-  authDomain: "Groomzy-fa8b0.firebaseapp.com",
-  projectId: "Groomzyfa8b0",
-  storageBucket: "Groomzyfa8b0.appspot.com",
+  authDomain: "mug-shop-fa8b0.firebaseapp.com",
+  projectId: "mug-shop-fa8b0",
+  storageBucket: "mug-shop-fa8b0.appspot.com",
   messagingSenderId: "591402500061",
   appId: "1:591402500061:web:ba5a787cbc94154b0a8034",
   measurementId: "G-DRJZD52FB3",
