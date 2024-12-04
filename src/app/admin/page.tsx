2623fa32b717217126c9ecbc322d2ee5d657fb0f
Nuwan Chamikara <www.gmnuwanchamikara@gmail.com>
2024-12-04 12:22:20 +0530

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,7 @@ import getOrders from "@/actions/getOrders";
 import getUsers from "@/actions/getUsers";
 import Container from "@/components/Container";
 import BarGraph from "./BarGraph";
-import getGraphData from "@/actions/getGraphdata";
+//import getGraphData from "@/actions/getGraphdata";
 
 const Admin = async () => {
   const products = await getProducts({ category: null });
