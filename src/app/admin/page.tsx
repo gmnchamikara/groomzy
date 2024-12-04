1ba0521eb750730f9555eabcfc42a41d6578866b
Nuwan Chamikara <www.gmnuwanchamikara@gmail.com>
2024-12-04 12:24:25 +0530

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -10,14 +10,14 @@ const Admin = async () => {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
-  const graphData = await getGraphData();
+  //const graphData = await getGraphData();
 
   return (
     <div className="pt-8">
       <Container>
         <Summary products={products} orders={orders} users={users} />
         <div className="mt-4 mx-auto max-w-[1150px]">
-          <BarGraph data={graphData}/>
+          {/* <BarGraph data={graphData}/> */}
         </div>
       </Container>
     </div>
